886b9a73f1fed503f5b059cf9afe2f9b6800f4f1
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-03 13:35:44 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -4,10 +4,11 @@ _G.engine             = EngineFactory.new()
     :with_width(1920)
     :with_height(1080)
     :with_scale(4.0)
-    :with_fullscreen(true)
+    :with_fullscreen(false)
     :create()
 
-local entitymanager   = engine:entitymanager()
+--local entitymanager   = engine:entitymanager()
+local cassete         = engine:cassete()
 local resourcemanager = engine:resourcemanager()
 local scenemanager    = engine:scenemanager()
 
@@ -15,9 +16,12 @@ function setup()
   resourcemanager:prefetch({ "blobs/red.png" })
   scenemanager:set("1")
 
-  -- local hammer = scenemanager:get("hammer")
+  -- local hammer = scenemanager:get("hammer 1")
 
   -- scenemanager:destroy() -- or set a new
+  --
+  cassete:set("foo", "bar")
+  print(cassete:get("foo"))
 end
 
 function loop()
