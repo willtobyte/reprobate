464ab83865844ab16aefff065c0ca075331a965d
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-05 11:04:22 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -9,20 +9,31 @@ _G.engine             = EngineFactory.new()
 
 --local entitymanager   = engine:entitymanager()
 local cassete         = engine:cassete()
+local fontfactory     = engine:fontfactory()
 local resourcemanager = engine:resourcemanager()
 local scenemanager    = engine:scenemanager()
+local overlay         = engine:overlay()
+
+local label           = nil
 
 function setup()
-  resourcemanager:prefetch({ "blobs/red.png" })
+  resourcemanager:prefetch({ "blobs/horn.png", "blobs/red.png" })
   scenemanager:set("1")
 
   -- local hammer = scenemanager:get("hammer 1")
 
   -- scenemanager:destroy() -- or set a new
   --
-  cassete:set("foo", "bar")
-  local value = cassete:get("foo")
-  print("value " .. value)
+
+  overlay.cursor:set("horn")
+
+  cassete:set("numbers", { 1, 2, 3 })
+  local value = cassete:get("numbers")
+  print("numbers: " .. table.concat(value, ", "))
+
+  label = overlay:create(WidgetType.label)
+  label.font = fontfactory:get("fixedsys")
+  label:set("Hello world", 10, 10)
 end
 
 function loop()
