4c0d07c8b6de84e548f059fc4e5bbfe189b21163
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-11 16:50:23 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -8,45 +8,31 @@ _G.engine             = EngineFactory.new()
     :create()
 
 local entitymanager   = engine:entitymanager()
-local cassete         = engine:cassete()
+-- local cassete         = engine:cassete()
 local fontfactory     = engine:fontfactory()
 local resourcemanager = engine:resourcemanager()
 local scenemanager    = engine:scenemanager()
 local overlay         = engine:overlay()
 
+local scene           = nil
 local label           = nil
-local player          = nil
 
 function setup()
-  resourcemanager:prefetch({ "fonts/fixedsys.json", "blobs/horn.png", "blobs/player.png", "blobs/red.png" })
-  scenemanager:set("1")
-
-  player = entitymanager:spawn("player")
-  player.placement:set(40, 40)
-  player.action:set("idle")
-
-  player:on_touch(function()
-    print("player was touched")
-  end)
-  -- local hammer = scenemanager:get("hammer 1")
-
-  -- scenemanager:destroy() -- or set a new
-  --
+  resourcemanager:prefetch({ "blobs/fixedsys.png", "blobs/horn.png" })
 
   overlay.cursor:set("horn")
-
-  -- overlay.cursor:dispatch("hit")
-
-  cassete:set("numbers", { 1, 2, 3 })
-  local value = cassete:get("numbers")
-  print("numbers: " .. table.concat(value, ", "))
 
   label = overlay:create(WidgetType.label)
   label.font = fontfactory:get("fixedsys")
   label:set("Hello world", 10, 10)
+
+  scene = require("scenes/1").construct(scenemanager, entitymanager)
 end
 
-function loop()
+function loop(delta)
+  if scene then
+    scene:loop(delta)
+  end
 end
 
 function run()
