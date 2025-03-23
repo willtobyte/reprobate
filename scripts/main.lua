3e431b083c06dd2ebd340d20ce0752473d78b211
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-23 13:36:01 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -4,7 +4,7 @@ _G.engine             = EngineFactory.new()
     :with_width(1920)
     :with_height(1080)
     :with_scale(4.0)
-    :with_fullscreen(false)
+    :with_fullscreen(true)
     :create()
 
 local entitymanager   = engine:entitymanager()
@@ -14,7 +14,10 @@ local resourcemanager = engine:resourcemanager()
 local scenemanager    = engine:scenemanager()
 local overlay         = engine:overlay()
 
+local io              = Socket.new()
+
 local label           = nil
+local lettercoro      = nil
 
 function setup()
   resourcemanager:prefetch({ "fonts/fixedsys.json", "blobs/horn.png", "blobs/player.png", "blobs/red.png" })
@@ -27,15 +30,40 @@ function setup()
 
   scenemanager:set("1")
 
-  local player = scenemanager:grab("le player")
+  local player = scenemanager:grab("player")
   player:on_touch(function()
-    overlay:dispatch(WidgetType.cursor, "hit")
+    overlay:dispatch(WidgetType.cursor, "damage")
   end)
 
-  print(player)
+  -- player = nil
+  -- scenemanager:set("2")
+  --
+  io:on("online", function(data)
+    print("Online " .. data.clients)
+  end)
+
+  io:connect()
+
+  lettercoro = coroutine.create(function()
+    local text = "Hello world"
+    local result = ""
+    for i = 1, #text do
+      result = result .. text:sub(i, i)
+      label:set(result, 10, 10)
+
+      local timer = 0
+      while timer < 0.1 do
+        local dt = coroutine.yield()
+        timer = timer + dt
+      end
+    end
+  end)
 end
 
 function loop(delta)
+  if lettercoro and coroutine.status(lettercoro) ~= "dead" then
+    coroutine.resume(lettercoro, delta)
+  end
 end
 
 function run()
