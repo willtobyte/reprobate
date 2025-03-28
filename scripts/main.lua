de08fd7973ccf0e0e7dcb12e2b832eff7426e7a5
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-28 09:13:04 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,7 +1,8 @@
 ---@diagnostic disable: undefined-global, undefined-field, lowercase-global
 
 --
--- Hello, magnificent curious one! Thank you for stopping by. If you’d like to chat, feel free to email me at rodrigo@delduca.org.
+-- Hello, magnificent curious one! Thank you for stopping by.
+-- If you’d like to chat, feel free to email me at rodrigo@delduca.org.
 --
 
 _G.engine             = EngineFactory.new()
