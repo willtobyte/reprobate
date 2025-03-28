065c2cd0fd0a8b9ed2d711a01f66009c88c076a1
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-28 11:15:57 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -53,7 +53,7 @@ function setup()
   scenemanager:on_enter("1", function()
     pool.label = overlay:create(WidgetType.label)
     pool.label.font = fontfactory:get("fixedsys")
-    pool.label:set("Run", 40, 10)
+    pool.label:set("Run", 40, 40)
 
     pool.player = scenemanager:grab("player")
     pool.player:on_touch(function()
@@ -61,7 +61,7 @@ function setup()
     end)
   end)
 
-  scenemanager:on_leave("0", function()
+  scenemanager:on_leave("1", function()
     overlay:destroy(pool.label)
 
     pool.player = nil
