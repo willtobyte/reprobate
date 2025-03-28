b03b2a081598cdf997f43cbc5bcf81df8abf8f74
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-28 09:12:51 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,23 +1,34 @@
 ---@diagnostic disable: undefined-global, undefined-field, lowercase-global
+
+--
+-- Hello, magnificent curious one! Thank you for stopping by. If you’d like to chat, feel free to email me at rodrigo@delduca.org.
+--
+
 _G.engine             = EngineFactory.new()
     :with_title("Reprobate")
     :with_width(1920)
     :with_height(1080)
     :with_scale(4.0)
-    :with_fullscreen(true)
+    :with_fullscreen(false)
     :create()
 
 local entitymanager   = engine:entitymanager()
--- local cassete         = engine:cassete()
 local fontfactory     = engine:fontfactory()
 local resourcemanager = engine:resourcemanager()
 local scenemanager    = engine:scenemanager()
 local overlay         = engine:overlay()
+local canvas          = engine:canvas()
 
 local label           = nil
+local pool            = {}
 
 function setup()
-  resourcemanager:prefetch({ "fonts/fixedsys.json", "blobs/horn.png", "blobs/player.png", "blobs/red.png" })
+  resourcemanager:prefetch({
+    "fonts/fixedsys.json",
+    "blobs/horn.png",
+    "blobs/player.png",
+    "blobs/red.png"
+  })
 
   overlay.cursor:set("horn")
 
@@ -25,18 +36,49 @@ function setup()
   label.font = fontfactory:get("fixedsys")
   label:set("Hello world", 10, 10)
 
-  scenemanager:set("1")
-
-  local player = scenemanager:grab("player")
-  player:on_touch(function()
-    overlay:dispatch(WidgetType.cursor, "damage")
+  scenemanager:on_enter("0", function()
+    print("Entering scene 0")
   end)
 
-  -- player = nil
-  -- scenemanager:set("2")
+  scenemanager:on_leave("0", function()
+    print("Leaving scene 0")
+    pool.button = nil
+  end)
+
+  scenemanager:on_enter("1", function()
+    print("Entering scene 1")
+    pool.player = scenemanager:grab("player")
+    pool.player:on_touch(function()
+      overlay:dispatch(WidgetType.cursor, "damage")
+    end)
+  end)
+
+  scenemanager:set("0")
+
+  pool.button = scenemanager:grab("button")
+  pool.button:on_touch(function()
+    scenemanager:set("1")
+  end)
 end
 
 function loop(delta)
+  local w, h, bs = 480, 270, 4
+  local green, transparent = 0xFF00FF00, 0x00000000
+  local total = w * h
+  local pixels = {}
+
+  for i = 1, total do
+    pixels[i] = green
+  end
+
+  for y = bs, h - bs - 1 do
+    local base = y * w
+    for x = bs, w - bs - 1 do
+      pixels[base + x + 1] = transparent
+    end
+  end
+
+  canvas.pixels = pixels
 end
 
 function run()
