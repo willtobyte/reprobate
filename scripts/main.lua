8d0950e4bd0909d45006cbe07aa267a681a41699
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-12 10:01:59 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -10,8 +10,15 @@ local resourcemanager = engine:resourcemanager()
 local scenemanager = engine:scenemanager()
 local overlay = engine:overlay()
 local fontfactory = engine:fontfactory()
+local overlay = engine:overlay()
+
+local label = nil
 
 function setup()
+  label = overlay:create(WidgetType.label)
+  label.font = fontfactory:get("verminvibes")
+  label:set("Rodrigo Delduca! 666", 0, 0)
+
   resourcemanager:prefetch()
 
   overlay.cursor:set("horn")
