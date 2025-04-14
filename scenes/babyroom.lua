86242ec5f062d9c9f2d649182676cf7eac930b09
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-14 09:36:21 -0300

    Work in progress

--- scenes/babyroom.lua
+++ scenes/babyroom.lua
@@ -1,48 +1,21 @@
 local noise = require("effects/noise")
+local writter = require("helpers/writter")
 
 local scene = {}
-
 local pool = {}
 
-local overlay = engine:overlay()
 local cassette = engine:cassette()
-local fontfactory = engine:fontfactory()
 local scenemanager = engine:scenemanager()
 local timermanager = engine:timermanager()
 local soundmanager = engine:soundmanager()
 
-pool.label = nil
-
-local function write(text, x, y)
-  if pool.timer then
-    timermanager:clear(pool.timer)
-    pool.timer = nil
-  end
-
-  pool.text = text
-  pool.index = 0
-  pool.label:set("", x, y)
-
-  local function tick()
-    pool.index = pool.index + 1
-    pool.label:set(text:sub(1, pool.index), x, y)
-    if pool.index >= #text then
-      timermanager:clear(pool.timer)
-      pool.timer = nil
-    end
-  end
-
-  pool.timer = timermanager:set(100, tick)
-end
-
 function scene.on_enter()
-  pool.label = overlay:create(WidgetType.label)
-  pool.label.font = fontfactory:get("evilvampire") -- "kreepykrawly"
+  noise.init()
 
   pool.timers = {}
 
   for _, o in ipairs({
-    { name = "car",  min = 3, max = 8, action = "run" },
+    { name = "car", min = 3, max = 8, action = "run" },
     { name = "bear", min = 2, max = 4, action = "blink" },
     { name = "clown", min = 6, max = 8, action = "blink" },
     { name = "robot", min = 3, max = 6, action = "shake" },
@@ -61,7 +34,7 @@ function scene.on_enter()
     { name = "crucifix", sound = "wind", damage = true },
     { name = "gijoe", sound = "door" },
     { name = "nintendo", sound = "metal" },
-    { name = "playboy", sound = "gore" }
+    { name = "playboy", sound = "gore" },
   }) do
     local key = "babyroom/" .. item.name
     pool[item.name] = scene:get(item.name)
@@ -80,18 +53,16 @@ function scene.on_enter()
   end
 
   pool.beelzebuuth = scene:get("beelzebuuth")
-  pool.beelzebuuth.action:unset()
 
+  -- Uncomment to enable the beelzebuuth summon event after 6.66*6000ms
   -- local id = timermanager:set(6.66 * 6000, function()
   --   soundmanager:play("scream")
   --   pool.beelzebuuth.action:set("summon")
   -- end)
   -- table.insert(pool.timers, id)
 
-  noise.init()
-
-  noise.on_end(function ()
-    write("I drown your holiness in the Acheron of my soul", 3, 3) -- 47
+  noise.on_end(function()
+    writter.write("I drown your holiness in the Acheron of my soul", 3, 3)
   end)
 end
 
@@ -104,15 +75,13 @@ function scene.on_leave()
   for _, id in ipairs(pool.timers) do
     timermanager:clear(id)
   end
-
-  for k in pairs(pool) do pool[k] = nil end
+  for k in pairs(pool) do
+    pool[k] = nil
+  end
 end
 
 function scene.on_touch()
-  -- Depois de 3-5 touchs aparece o beelzebuuth
-
   pool.label:clear()
-
   if pool.timer then
     timermanager:clear(pool.timer)
     pool.timer = nil
---
+++ scripts/helpers/writter.lua
@@ -0,0 +1,49 @@
+local writter = {}
+
+local fontfactory = engine:fontfactory()
+local overlay = engine:overlay()
+local timermanager = engine:timermanager()
+
+local pool = {}
+
+pool.label = nil
+pool.timer = nil
+pool.timeout = nil
+pool.callback = nil
+
+pool.label = overlay:create(WidgetType.label)
+pool.label.font = fontfactory:get("evilvampire")
+
+function writter.on_finish(timeout, callback)
+  pool.timeout = timeout
+  pool.callback = callback
+end
+
+function writter.write(text, x, y)
+  if pool.timer then
+    timermanager:clear(pool.timer)
+    pool.timer = nil
+  end
+
+  pool.text = text
+  pool.index = 0
+  pool.label:set("", x, y)
+
+  local function tick()
+    pool.index = pool.index + 1
+    pool.label:set(text:sub(1, pool.index), x, y)
+    if pool.index >= #text then
+      timermanager:clear(pool.timer)
+      pool.timer = nil
+
+      if pool.callback then
+        timermanager:singleshot(pool.timeout, pool.callback)
+      end
+    end
+  end
+
+  local timeout = 100
+  pool.timer = timermanager:set(timeout, tick)
+end
+
+return writter
