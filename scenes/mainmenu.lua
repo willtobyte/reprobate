334657059a80a3235b7df09beba0ab6fbeff8184
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-16 07:27:53 -0300

    Work in progress

--- scenes/mainmenu.lua
+++ scenes/mainmenu.lua
@@ -1,14 +1,27 @@
 local scene = {}
 
+local scenemanager = engine:scenemanager()
+
 local pool = {}
 
 function scene.on_enter()
+  local play = scene:get("play")
+  play:on_touch(function ()
+    scenemanager:set("babyroom")
+  end)
+
+  pool.headbanger = scene:get("headbanger")
 end
 
 function scene.on_touch()
 end
 
 function scene.on_motion(x, y)
+  if x > 240 then -- 480 / 2
+    pool.headbanger.action:set("right")
+  else
+    pool.headbanger.action:set("left")
+  end
 end
 
 return scene
