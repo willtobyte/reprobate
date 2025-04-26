fdb951c49d6b2572d81c91d892d9b1a699aa51e2
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-26 09:57:03 -0300

    Work in progress

--- scenes/babyroom.lua
+++ scenes/babyroom.lua
@@ -2,6 +2,7 @@ local scene = {}
 
 local noise = require("effects/noise")
 local scribe = require("helpers/scribe")
+local touch = require("helpers/touch")
 
 local pool = {}
 local lock = false
@@ -83,30 +84,7 @@ function scene.on_enter()
         cassette:set(key, true)
         pool.collected[name] = true
 
-        local duration = 500
-        local interval = 32
-        local steps = math.floor(duration / interval)
-        local step = 0
-
-        local is = self.scale
-        local fs = is * 1.2
-
-        local ia = self.alpha
-        local fa = 0
-
-        local id
-        id = timermanager:set(interval, function()
-          step = step + 1
-
-          local t = step / steps
-          self.scale = is + (fs - is) * t
-          self.alpha = math.floor(ia + (fa - ia) * t)
-
-          if step >= steps then
-            timermanager:clear(id)
-            self:hide()
-          end
-        end)
+        touch.disappear(self)
 
         for _, collected in pairs(pool.collected) do
           if not collected then
