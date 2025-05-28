c8782f7407e3eac1874c6da5188388e9ad64f941
Rodrigo Delduca <rodrigo@delduca.org>
2025-05-28 16:56:46 -0300

    Work in progress

--- scenes/pearintosh.lua
+++ scenes/pearintosh.lua
@@ -7,14 +7,21 @@ local overlay = engine:overlay()
 local pool = {}
 
 function scene.on_enter()
+  pool.prelude = [[
+PHOSPHORUS SOFTWARE 1983 (C)
+BASIC V1.6.3
+96128 BYTES FREE
+
+]]
+
   pool.program = ""
 
   pool.label = overlay:create(WidgetType.label)
   local font = fontfactory:get("fixedsys")
+  font.effect = FontEffect.cursor
   pool.label.font = font
-  -- TODO effect cursor
 
-  pool.label:set("", 3, 3)
+  pool.label:set(pool.prelude .. pool.program)
 end
 
 function scene.on_loop(delta)
@@ -25,37 +32,28 @@ function scene.on_leave()
 end
 
 function scene.on_text(text)
-  -- if not text:match("^[a-zA-Z0-9<>{}%[%]%(%)%+%-%.%*%&%^%%%$#@!~#@!]$") then
-  --   return
-  -- end
-
   pool.program = pool.program .. text
-
-  pool.label:set(pool.program)
+  pool.label:set(pool.prelude .. pool.program)
 end
 
 function scene.on_keypress(code)
   if code == KeyEvent.backspace and #pool.program > 0 then
     pool.program = pool.program:sub(1, -2)
-
-    pool.label:set(pool.program)
+    pool.label:set(pool.prelude .. pool.program)
     return
   end
 
   if code == KeyEvent.space then
     pool.program = pool.program .. " "
-
-    pool.label:set(pool.program)
+    pool.label:set(pool.prelude .. pool.program)
     return
   end
 
   if code == KeyEvent.enter then
     pool.program = pool.program .. "\n"
-
-    pool.label:set(pool.program)
+    pool.label:set(pool.prelude .. pool.program)
     return
   end
-
 end
 
 return scene
