508d285aa2d8452f78ae93a8d5a2b586eae9b60d
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-03 15:22:54 -0300

    Work in progress

--- scenes/whobuilt.lua
+++ scenes/whobuilt.lua
@@ -5,7 +5,7 @@ local scenemanager = engine:scenemanager()
 local pool = {}
 
 function scene.on_enter()
-  achievement:unlock("NEW_ACHIEVEMENT_1_7")
+  achievement:unlock("NEW_ACHIEVEMENT_1_8")
 
   pool.music = scene:get("theme", SceneType.effect)
   pool.music:play(true)
