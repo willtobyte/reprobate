d9b9dd7ca4531dbd988d0ed9479e80d4694a29ee
Rodrigo Delduca <rodrigo@delduca.org>
2025-07-09 10:08:04 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,3 +1,27 @@
+-- -- stylua: ignore
+-- _G.engine = EngineFactory.new()
+--   :with_title("Tela Vermelha")
+--   :with_width(1920)
+--   :with_height(1080)
+--   :with_scale(1.0)
+--   :with_fullscreen(false)
+--   :create()
+
+-- local canvas = engine:canvas()
+-- local width, height = 1920, 1080
+-- local pixels = {}
+
+-- function setup()
+--   local red = 0xFF0000FF
+--   for i = 1, width * height do
+--     pixels[i] = red
+--   end
+-- end
+
+-- function loop()
+--   canvas.pixels = pixels
+-- end
+
 -- stylua: ignore
 _G.engine = EngineFactory.new()
   :with_title("Reprobate")
