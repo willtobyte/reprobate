a38ba99ddc699cbb2c448520622b5a3690a17200
Rodrigo Delduca <rodrigo@delduca.org>
2025-07-19 17:23:05 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,33 +1,10 @@
--- -- stylua: ignore
--- _G.engine = EngineFactory.new()
---   :with_title("Benchmark")
---   :with_width(3920)
---   :with_height(4080)
---   :with_scale(1.0)
---   :with_fullscreen(false)
---   :create()
-
--- local canvas = engine:canvas()
--- local width, height = 3920, 4080
--- -- local pixels = {}
--- local red = string.char(0xFF, 0x00, 0x00, 0xFF)
--- local pixels = red:rep(width * height)
-
--- function setup()
--- end
-
--- function loop()
---   canvas.pixels = pixels
--- end
-
--- stylua: ignore
 _G.engine = EngineFactory.new()
-  :with_title("Reprobate")
-  :with_width(1920)
-  :with_height(1080)
-  :with_scale(4.0)
-  :with_fullscreen(true)
-  :create()
+	:with_title("Reprobate")
+	:with_width(1920)
+	:with_height(1080)
+	:with_scale(4.0)
+	:with_fullscreen(true)
+	:create()
 
 local scenemanager = engine:scenemanager()
 
@@ -45,7 +22,7 @@ function setup()
 
 	local stage = queryparam("stage", "mainmenu")
 
-	-- stage = "pixelslab"
+	-- stage = "mainmenu"
 
 	scenemanager:set(stage)
 end
