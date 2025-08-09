db27e8223afaa8e397f4842f6c1e6dc4f869504c
Rodrigo Delduca <rodrigo@delduca.org>
2025-08-09 09:27:43 -0300

    Work in progress

--- scenes/livingroom.lua
+++ scenes/livingroom.lua
@@ -2,9 +2,35 @@ local scene = {}
 
 local pool = {}
 
-function scene.on_enter() end
+local timermanager = engine:timermanager()
+
+local animations = {
+	window = { minimum = 4, maximum = 8, action = "lightning", message = "TODO..." },
+}
+
+function scene.on_enter()
+	pool.timers = {}
+
+	for name, settings in pairs(animations) do
+		local object = scene:get(name, SceneType.object)
+
+		local delay = math.random(settings.minimum, settings.maximum) * 1000
+
+		local id = timermanager:set(delay, function()
+			object.action = settings.action
+		end)
+
+		table.insert(pool.timers, id)
+
+		pool[name] = object
+	end
+end
 
 function scene.on_leave()
+	for _, id in ipairs(pool.timers) do
+		timermanager:clear(id)
+	end
+
 	for o in pairs(pool) do
 		pool[o] = nil
 	end
