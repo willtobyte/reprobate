fab65d7ecab413a560ac57826dd6d647362bba73
Rodrigo Delduca <rodrigo@delduca.org>
2025-08-26 09:15:35 -0300

    Work in progress

--- scenes/whobuilt.lua
+++ scenes/whobuilt.lua
@@ -5,60 +5,58 @@ local scenemanager = engine:scenemanager()
 local pool = {}
 
 function scene.on_enter()
-  achievement:unlock("NEW_ACHIEVEMENT_2_0")
+  local music = scene:get("theme", SceneType.effect)
+  music:play(true)
 
-  pool.music = scene:get("theme", SceneType.effect)
-  pool.music:play(true)
-
-  pool.symbols = scene:get("symbols", SceneType.object)
-  pool.goat = scene:get("goat", SceneType.effect)
-  pool.symbols:on_touch(function()
-    pool.goat:play()
+  local symbols = scene:get("symbols", SceneType.object)
+  local goat = scene:get("goat", SceneType.effect)
+  symbols:on_touch(function()
+    goat:play()
   end)
 
-  pool.back = scene:get("backbutton", SceneType.object)
+  local back = scene:get("backbutton", SceneType.object)
 
-  pool.back:on_hover(function()
-    pool.back.action = "hover"
+  back:on_hover(function(self)
+    self.action = "hover"
   end)
 
-  pool.back:on_unhover(function()
-    pool.back.action = "default"
+  back:on_unhover(function(self)
+    self.action = "default"
   end)
 
-  pool.back:on_touch(function()
+  back:on_touch(function()
     scenemanager:set("mainmenu")
   end)
 
-  pool.aline = scene:get("aline", SceneType.object)
+  local aline = scene:get("aline", SceneType.object)
 
-  pool.rodrigo = scene:get("rodrigo", SceneType.object)
+  local rodrigo = scene:get("rodrigo", SceneType.object)
 
-  pool.aline:on_hover(function()
-    pool.aline.action = "hover"
+  aline:on_hover(function(self)
+    self.action = "hover"
   end)
 
-  pool.aline:on_unhover(function()
-    pool.aline.action = "burning"
-    pool.rodrigo.action = nil
-    pool.rodrigo.action = "burning"
+  aline:on_unhover(function(self)
+    self.action = "burning"
+    rodrigo.action = nil
+    rodrigo.action = "burning"
   end)
 
-  pool.aline:on_touch(function()
+  aline:on_touch(function()
     openurl("https://linktr.ee/dandelion.pixelart")
   end)
 
-  pool.rodrigo:on_hover(function()
-    pool.rodrigo.action = "hover"
+  rodrigo:on_hover(function(self)
+    self.action = "hover"
   end)
 
-  pool.rodrigo:on_unhover(function()
-    pool.rodrigo.action = "burning"
-    pool.aline.action = nil
-    pool.aline.action = "burning"
+  rodrigo:on_unhover(function(self)
+    self.action = "burning"
+    aline.action = nil
+    aline.action = "burning"
   end)
 
-  pool.rodrigo:on_touch(function()
+  rodrigo:on_touch(function()
     openurl("https://rodrigodelduca.org")
   end)
 end
