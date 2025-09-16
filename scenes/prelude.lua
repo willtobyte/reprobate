cf1e744c2988d0733bd799624a04726e6c2f7a60
Rodrigo Delduca <rodrigo@delduca.org>
2025-09-16 11:03:09 -0300

    Work in progress

--- scenes/prelude.lua
+++ scenes/prelude.lua
@@ -1,7 +1,5 @@
 local scene = {}
 
-local sentinel = require("helpers/sentinel")
-
 local pool = {}
 
 local scenemanager = engine:scenemanager()
@@ -39,23 +37,6 @@ function scene.on_leave()
   end
 end
 
-local function attach_finalizer(t, ongc)
-  if type(t) ~= "table" or type(ongc) ~= "function" then
-    return
-  end
-  local np = rawget(_G, "newproxy")
-  if np then
-    local u = np(true)
-    getmetatable(u).__gc = ongc
-    t.__sentinel = u
-    return u
-  end
-  t.__sentinel = setmetatable({}, { __gc = ongc })
-  return t.__sentinel
-end
-
-sentinel(scene, function()
-  print("[GC] collected scene prelude")
-end)
+sentinel(scene, "prelude")
 
 return scene
