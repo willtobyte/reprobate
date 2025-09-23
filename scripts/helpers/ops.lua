4d708870b19086d2b01c0f17ab8f3741d22a3edb
Rodrigo Delduca <rodrigo@delduca.org>
2025-09-23 17:51:08 -0300

    Work in progress

--- scripts/helpers/ops.lua
+++ scripts/helpers/ops.lua
@@ -1,39 +1,39 @@
 local M = {}
 
 local function add(observable, delta)
-  local v = observable.value
-  if v == nil then
+  local value = observable.value
+  if value == nil then
     observable:set(delta)
     return
   end
-  observable:set(v + delta)
+  observable:set(value + delta)
 end
 
 function M.incr(observable)
   add(observable, 1)
 end
 
-function M.incrby(observable, n)
-  add(observable, n)
+function M.incrby(observable, number)
+  add(observable, number)
 end
 
 function M.decr(observable)
   add(observable, -1)
 end
 
-function M.decrby(observable, n)
-  add(observable, -n)
+function M.decrby(observable, number)
+  add(observable, -number)
 end
 
-function M.getset(observable, new_value)
+function M.getset(observable, new)
   local old = observable.value
-  observable:set(new_value)
+  observable:set(new)
   return old
 end
 
-function M.setnx(observable, v)
+function M.setnx(observable, value)
   if observable.value == nil then
-    observable:set(v)
+    observable:set(value)
     return true
   end
   return false
