43c1b2d82766a50f9c62d638bfb0c8eb4a1738f3
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-02 10:01:59 -0300

    Work in progress

--- scenes/pearintosh.lua
+++ scenes/pearintosh.lua
@@ -61,9 +61,11 @@ RUN TO EXECUTE, EXIT TO QUIT
     pool.halted = false
   end)
 
-  pentagram:on_finish(function()
+  pool.dialup = scene:get("dialup", SceneType.effect)
+
+  pool.dialup:on_end(function()
     pentagram:teardown()
-    print(">>>")
+    print(">>> ...")
     -- TODO
     -- scenemanager:set("minigame")
   end)
@@ -140,7 +142,9 @@ function scene.on_keypress(code)
           pool.pentagram = message == "666"
 
           if pool.pentagram then
+            achievement:unlock("ACH_IN_LEAGUE_WITH_SATAN") -- In League with Satan.
             pool.backcursor:hide()
+            pool.dialup:play()
           end
         end
 
