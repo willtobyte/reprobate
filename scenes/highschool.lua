5c0d67fa7e1e97a6bb8c459ef62c88f9862980fb
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-02 16:33:21 -0300

    Work in progress

--- scenes/highschool.lua
+++ scenes/highschool.lua
@@ -72,7 +72,7 @@ local objects = {
   redguy = {
     messages = {
       "The road of rebellion leads to inner power.",
-      "The Will to Potency.\nThe Will to Potency.\nThe Will to Potency.\nThe Will to Potency.", -- Always last.
+      "The Will to Potency.\nThe Will to Potency.\nThe Will to Potency.\nThe Will to Potency.", -- Always update `trigger_index`.
     },
     receivables = {
       ["HUD/playboy"] = {
