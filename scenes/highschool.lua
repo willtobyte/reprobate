b81b6246051a5b06ad5869bcabfce7a23acef575
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-03 19:28:09 -0300

    Work in progress

--- scenes/highschool.lua
+++ scenes/highschool.lua
@@ -45,7 +45,7 @@ local objects = {
     messages = {
       "Reactionary discourse from state-puppet teachers.",
       "Turn off the TV, forget your idols, and face yourself.",
-      "",
+      "Tune in to our pirate radio station on 43.2 FM.",
     },
     receivables = {
       ["HUD/playboy"] = {
