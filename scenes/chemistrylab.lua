8729491cec78fd24ec7552aca27cab193537b47b
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-28 13:50:57 -0300

    Work in progress

--- scenes/chemistrylab.lua
+++ scenes/chemistrylab.lua
@@ -4,20 +4,12 @@ local pool = {}
 
 function scene.on_enter() end
 
-function scene.on_motion(x, y)
-  lantern:motion(x, y)
-end
+function scene.on_motion(x, y) end
 
-function scene.on_loop()
-  lantern:loop()
-end
+function scene.on_loop() end
 
 function scene.on_leave()
-  lantern:teardown()
-
-  for key in next, pool do
-    pool[key] = nil
-  end
+  pool = {}
 end
 
 sentinel(scene, "chemistrylab")
