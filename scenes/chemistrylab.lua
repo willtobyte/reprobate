d76ad9704ff5079d88c1c5449477ca0b5a6de6e5
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-28 15:51:44 -0300

    Work in progress

--- scenes/chemistrylab.lua
+++ scenes/chemistrylab.lua
@@ -2,9 +2,13 @@ local scene = {}
 
 local pool = {}
 
-function scene.on_enter() end
+function scene.on_enter()
+  pool.lightmask = scene:get("lightmask", SceneType.object)
+end
 
-function scene.on_motion(x, y) end
+function scene.on_motion(x, y)
+  pool.lightmask.placement = { x - 480, y - 322.5 }
+end
 
 function scene.on_loop() end
 
