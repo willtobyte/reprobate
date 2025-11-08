fe60e0b72497c11fa62951c6f080e71b4d27855f
Rodrigo Delduca <rodrigo@delduca.org>
2025-11-08 17:36:07 -0300

    Work in progress

--- scripts/helpers/jump.lua
+++ scripts/helpers/jump.lua
@@ -1,6 +1,9 @@
 local M = {}
 
 function M.to(name, delay)
+  assert(type(name) == "string" and #name > 0, "scene name must be a non-empty string")
+  assert(delay == nil or (type(delay) == "number" and delay >= 0), "delay must be a non-negative number")
+
   delay = delay or 100
   return function()
     timermanager:singleshot(delay, function()
