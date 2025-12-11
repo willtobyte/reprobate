0174b2a0de4c59947e8fffc1a0877193ee04a4e6
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-11 17:08:37 -0300

    Work in progress

--- scripts/camera.lua
+++ scripts/camera.lua
@@ -2,7 +2,7 @@ local camera = {}
 
 function camera.calculate(delta)
   local x, y, vw, vh = 0, 0, 100, 200 -- TODO
-  return quad.new(x, y, vw, vh)
+  return Quad.new(x, y, vw, vh)
 end
 
 return camera
