53d28958455f46075a7212166f6d4ec674b9c62b
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-20 19:40:23 -0300

    Work in progress

--- scripts/globals.lua
+++ scripts/globals.lua
@@ -146,3 +146,18 @@ function jump.to(name, delay)
     end)
   end
 end
+
+function async(fn)
+  local co = coroutine.create(fn)
+  local function resume()
+    local ok, delay = coroutine.resume(co)
+    if ok and delay and coroutine.status(co) ~= "dead" then
+      timermanager:singleshot(delay, resume)
+    end
+  end
+  resume()
+end
+
+function wait(ms)
+  coroutine.yield(ms)
+end
