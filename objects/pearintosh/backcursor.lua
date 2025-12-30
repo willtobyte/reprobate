8d13c9c2d1f75c19a302f8d9f9a6e0b9677bf906
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-30 16:04:34 -0300

    Work in progress

--- objects/pearintosh/backcursor.lua
+++ objects/pearintosh/backcursor.lua
@@ -1,12 +1,11 @@
 return {
-  on_spawn = function()
-    self.action = "default"
-  end,
   on_hover = function()
     self.action = "hover"
   end,
+
   on_unhover = function()
     self.action = "default"
   end,
+
   on_touch = jump.to("highschool"),
 }
