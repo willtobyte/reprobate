deafafc77f5fe57502fe9f128b4a646a61eda0c3
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-31 19:41:59 -0300

    Work in progress

--- objects/livingroom/cabinetdoor.lua
+++ objects/livingroom/cabinetdoor.lua
@@ -3,22 +3,26 @@ local tweens = require("helpers/tweens")
 local say = require("helpers/scribe").say
 
 return {
-  on_spawn = function()
-    if state.cabinetdoor then
-      self.action = "open"
-    end
-  end,
+	on_spawn = function()
+		if not state.cabinetdoor then
+			return
+		end
+		self.action = "open"
+		if not state.voodoodoll then
+			pool.voodoodoll.action = "default"
+		end
+	end,
 
-  on_touch = function()
-    if state.cabinetdoor then
-      return
-    end
-    self:on_touch(nil)
-    state.cabinetdoor = true
-    self.action = "open"
-    pool.voodoodoll.action = "default"
-    pool.voodoodoll.alpha = 0
-    tweens.appear.voodoodoll = tween.new(1, pool.voodoodoll, { alpha = 255 })
-    say("The doll is not yours, it belongs to the loa that rides it.", 3, 3, 3000)
-  end,
+	on_touch = function()
+		if state.cabinetdoor then
+			return
+		end
+		self:on_touch(nil)
+		state.cabinetdoor = true
+		self.action = "open"
+		pool.voodoodoll.action = "default"
+		pool.voodoodoll.alpha = 0
+		tweens.appear.voodoodoll = tween.new(1, pool.voodoodoll, { alpha = 255 })
+		say("The doll is not yours, it belongs to the loa that rides it.", 3, 3, 3000)
+	end,
 }
