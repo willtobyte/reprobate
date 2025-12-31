bbeee3b8bf0ecab77fcb2a48b9a620efa022c958
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-31 11:14:57 -0300

    Work in progress

--- scenes/highschool.lua
+++ scenes/highschool.lua
@@ -6,39 +6,40 @@ local tweens = require("helpers/tweens")
 local scribe = require("helpers/scribe")
 
 function scene.on_enter()
-  state.system.stage = "highschool"
+	state.system.stage = "highschool"
 
-  transition({
-    destroy = { "mainmenu", "whobuilt", "livingroom" },
-    register = { "pearintosh" },
-  })
+	transition({
+		destroy = { "mainmenu", "whobuilt", "livingroom" },
+		register = { "pearintosh" },
+	})
 
-  local magazine = pool["HUD/playboy"]
-  pool.inventory = Inventory.new(pool.layout, pool.boy, { magazine })
+	local magazine = pool["HUD/playboy"]
 
-  if not state.sourcecode then
-    return
-  end
-  pool.minisourcecode.action = "default"
-  magazine.action = nil
+	pool.inventory = Inventory.new(pool.layout, pool.boy, { magazine })
+
+	if not state.sourcecode then
+		return
+	end
+
+	pool.minisourcecode.action = "default"
+
+	magazine.action = nil
 end
 
 function scene.on_motion(x, y)
-  pool.inventory.motion(x, y)
+	pool.inventory.motion(x, y)
 end
 
-function scene.on_touch() end
-
 function scene.on_loop(delta)
-  scribe.loop(delta)
-  pool.inventory.loop(delta)
-  tweens.loop(delta)
+	scribe.loop(delta)
+	pool.inventory.loop(delta)
+	tweens.loop(delta)
 end
 
 function scene.on_leave()
-  scribe.clear()
-  tweens.teardown()
-  pool.inventory.teardown()
+	scribe.clear()
+	tweens.teardown()
+	pool.inventory.teardown()
 end
 
 sentinel(scene, "highschool")
--- scripts/helpers/collectible.lua
+++ scripts/helpers/collectible.lua
@@ -2,37 +2,42 @@ local tween = require("library/tween")
 local tweens = require("helpers/tweens")
 
 local function collectible(name, options)
-  local damage = options and options.damage
-  local hud = options and options.hud
+	local damage = options and options.damage
+	local hud = options and options.hud
 
-  return {
-    on_spawn = function()
-      if not state[name] then
-        return
-      end
-      pool[name].visible = false
-      if hud then
-        pool["HUD/" .. name].action = "default"
-      end
-    end,
+	return {
+		on_spawn = function()
+			if not state[name] then
+				return
+			end
 
-    on_touch = function()
-      if state[name] then
-        return
-      end
-      local object = pool[name]
-      object:on_touch(nil)
-      state[name] = true
-      if damage then
-        overlay:dispatch(WidgetType.cursor, "damage")
-      end
-      tweens.disappear[name] = tween.new(1, object, { alpha = 0, angle = 360, scale = 1.6 }, "inOutQuad")
-      if hud then
-        pool["HUD/" .. name].action = "default"
-      end
-      emit.collected(name)
-    end,
-  }
+			pool[name].visible = false
+			if hud then
+				pool["HUD/" .. name].action = "default"
+			end
+		end,
+
+		on_touch = function()
+			if state[name] then
+				return
+			end
+
+			local object = pool[name]
+			object:on_touch(nil)
+			state[name] = true
+
+			if damage then
+				overlay:dispatch(WidgetType.cursor, "damage")
+			end
+
+			tweens.disappear[name] = tween.new(1, object, { alpha = 0, angle = 360, scale = 1.6 }, "inOutQuad")
+			if hud then
+				pool["HUD/" .. name].action = "default"
+			end
+
+			emit.collected(name)
+		end,
+	}
 end
 
 return collectible
