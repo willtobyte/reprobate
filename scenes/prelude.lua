6dc8b1dcf9649e330cf8ae5977a878b297bb6898
Rodrigo Delduca <rodrigo@delduca.org>
2026-01-21 10:23:57 -0300

    Work in progress

--- scenes/prelude.lua
+++ scenes/prelude.lua
@@ -8,8 +8,6 @@ end
 function scene.on_touch()
   pool.click:play()
 
-  achievement:unlock("ACH_THE_FIRST_OF_MANY")
-
   pool.clicks = pool.clicks + 1
   if pool.clicks >= 10 then
     achievement:unlock("ACH_CLICK_FOREHEAD") -- How about trying to click with your forehead?
