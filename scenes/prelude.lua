7c1c2f131b53477b194af3cbc7b72172fd1e4342
Rodrigo Delduca <rodrigo@delduca.org>
2026-02-03 15:15:44 -0300

    Work in progress

--- scenes/prelude.lua
+++ scenes/prelude.lua
@@ -3,18 +3,6 @@ local scene = {}
 function scene.on_enter()
   transition({ register = { "mainmenu", "whobuilt" } })
   pool.clicks = 0
-
-  --
-  achievement:unlock("NEW_ACHIEVEMENT_4_0")
-
-  print("Persona Name: " .. user:persona())
-
-  local friends = user:friends()
-  for i = 1, #friends do
-    local friend = friends[i]
-    print("Friend ID: " .. tostring(friend.first) .. ", Name: " .. friend.second)
-  end
-  --
 end
 
 function scene.on_touch()
