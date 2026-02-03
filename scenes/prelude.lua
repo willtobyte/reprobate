92b285061e500ee52cb1df91ce105d5f7b17581b
Rodrigo Delduca <rodrigo@delduca.org>
2026-02-03 15:26:05 -0300

    Work in progress

--- scenes/prelude.lua
+++ scenes/prelude.lua
@@ -3,6 +3,14 @@ local scene = {}
 function scene.on_enter()
   transition({ register = { "mainmenu", "whobuilt" } })
   pool.clicks = 0
+
+  print("Persona: " .. user:persona())
+
+  local buddies = user:buddies()
+  for i = 1, #buddies do
+    local buddy = buddies[i]
+    print("Buddy ID: " .. tostring(buddy.id) .. ", Name: " .. buddy.name)
+  end
 end
 
 function scene.on_touch()
