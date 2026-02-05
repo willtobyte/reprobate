8359f1442d78ca72ced6623f7ca52829fd246708
Rodrigo Delduca <rodrigo@delduca.org>
2026-02-05 11:25:41 -0300

    Work in progress

--- objects/fourforcescomeforth/blood.lua
+++ objects/fourforcescomeforth/blood.lua
@@ -1,1 +1,17 @@
-return {}
+return {
+  on_spawn = function()
+    print("On init")
+    self.life = 5
+  end,
+
+  on_damage = function()
+    print("On damage")
+
+    self.life = self.life - 1
+    if self.life < 0 then
+      return
+    end
+
+    self.action = tostring(self.life)
+  end,
+}
--- scenes/fourforcescomeforth.lua
+++ scenes/fourforcescomeforth.lua
@@ -6,6 +6,10 @@ function scene.on_motion(x, y)
   --
 end
 
+function scene.on_touch()
+  pool.blood.damage()
+end
+
 sentinel(scene, "fourforcescomeforth")
 
 return scene
