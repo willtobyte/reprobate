ccc70f67ad7ad31985fe489a1cebedbc07423472
Rodrigo Delduca <rodrigo@delduca.org>
2026-02-05 11:26:21 -0300

    Work in progress

--- objects/fourforcescomeforth/blood.lua
+++ objects/fourforcescomeforth/blood.lua
@@ -1,12 +1,9 @@
 return {
   on_spawn = function()
-    print("On init")
     self.life = 5
   end,
 
   on_damage = function()
-    print("On damage")
-
     self.life = self.life - 1
     if self.life < 0 then
       return
