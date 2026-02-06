d90d70fbde12d5c60d9e007fc878275123e7bf67
Rodrigo Delduca <rodrigo@delduca.org>
2026-02-06 11:52:47 -0300

    Work in progress

--- objects/fourforcescomeforth/projectile.lua
+++ objects/fourforcescomeforth/projectile.lua
@@ -1,1 +1,28 @@
-return {}
+local mapping = {
+  satan = { x = 1, y = -1 },
+  belial = { x = 1, y = -1 },
+  lucifer = { x = -1, y = 1 },
+  leviathan = { x = -1, y = 1 },
+}
+
+return {
+  on_spawn = function()
+    self.velocity = { x = 50, y = 50 }
+  end,
+
+  on_collision = function(_, kind)
+    if kind == "iconofhypocrisy" then
+      pool.health.damage()
+    end
+
+    local reflect = mapping[kind]
+    if not reflect then
+      return
+    end
+
+    self.velocity = {
+      x = self.velocity.x * reflect.x,
+      y = self.velocity.y * reflect.y,
+    }
+  end,
+}
